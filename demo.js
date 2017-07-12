05557f1e80feb4049508a8f2da33508e9cab5527
Joshua McKinney <mckinneyj@byui.edu>
2017-07-12 17:30:43 -0600

    added other api call to demo.js

--- demo.js
+++ demo.js
@@ -5,3 +5,11 @@ d2lScrape.getCourseHtmlPages(10011, function (err, pages) {
     console.log("pages.successfulPages:", pages.successfulPages);
     console.log("pages.errorPages:", pages.errorPages);
 })
+
+d2lScrape.getTopicsWithUrlFromToc(10011, function (err, topics) {
+    if (err) {
+        console.log("err:", err);
+    }
+
+    console.log("topics:", topics);
+})
